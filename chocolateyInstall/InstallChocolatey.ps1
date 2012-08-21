38ab34cdd188711144ff9fcf24404ef68f703e00
piers7 <male@piers7.com>
2012-08-21 09:55:57 +0800

    Add optional proxy auth to install script
    Lets installer be usable on corporate networks etc... if the correct bootstrap cmdline is used

--- chocolateyInstall/InstallChocolatey.ps1
+++ chocolateyInstall/InstallChocolatey.ps1
@@ -1,3 +1,6 @@
+param(
+    [switch] $defaultProxyAuth # Supply default credentials to proxy servers
+)
 # ==============================================================================
 # 
 # Fervent Coder Copyright � 2011 - Released under the Apache 2.0 License
@@ -28,6 +31,10 @@ $file = Join-Path $tempDir "chocolatey.zip"
 # download the package
 Write-Host "Downloading $url to $file"
 $downloader = new-object System.Net.WebClient
+if($defaultProxyAuth){
+    # Make the install work when on corporate networks etc...
+    $downloader.Proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials;
+}
 $downloader.DownloadFile($url, $file)
 
 # unzip the package
