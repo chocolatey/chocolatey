14e47e346e973de427a0071d92e12270543182ef
Redsandro Media <redsandro@gmail.com>
2012-12-21 03:52:31 +0100

    Cleaned up comments

--- src/helpers/functions/Get-FilenameFromRegex.ps1
+++ src/helpers/functions/Get-FilenameFromRegex.ps1
@@ -16,28 +16,21 @@ function Get-FilenameFromRegex {
 	)
 
 	try {
-		# http://www.joejoesoft.com/sim/b95dfb866231b4ca38b837dca4b65e11/userupload/8/files/rmv303.zip
-		# get hash from $zipUrl html
-		# DOWNLOAD: <a href="/cms/file.php?f=userupload/8/files/rmv303.zip&amp;c=b95dfb866231b4ca38b837dca4b65e11">Click to download userupload/8/files/rmv303.zip</a><br>
-		# /files/rmv303.zip&amp;c=b95dfb866231b4ca38b837dca4b65e11
-		# /files/rmv303.zip&amp;c=([a-z0-9])
-		
+
 		## Example # $source_url = "http://www.joejoesoft.com/vcms/hot/userupload/8/files/rmv303.zip"
+		## Example # $find = '/cms/file.php\?f=userupload/8/files/rmv303.zip&amp;c=([\w\d]+)'
+		## Example # $replace = 'http://www.joejoesoft.com/sim/$1/userupload/8/files/rmv303.zip'		
+
+		# Download and open download page
 		$tempfile = "chocolatey_temp_download.html"
 		$wc = new-object system.net.webclient
 		$wc.UseDefaultCredentials = $true
 		$wc.downloadfile($source_url, $tempfile)
 		$source = Get-Content $tempfile
 		
-		## Example # $find = '/cms/file.php\?f=userupload/8/files/rmv303.zip&amp;c=([\w\d]+)'
-		
-		## Example # $replace = 'http://www.joejoesoft.com/sim/$1/userupload/8/files/rmv303.zip'
-		# Replace references with $matches $1 $2 $n -> $matches[1] $matches[2] $matches[n]
-		#$replace = $replace -creplace "\$`(\d+)",'$matches[$1]'
-		
 		$nothing = ''+$source -cmatch $find # Get the matches, prevent output
 		# Replace Match-references with previous matches: $1 $2 -> $matches[1] $matches[2]
-		# $final = $replace -creplace "\$`(\d+)",$matches[$1]
+		#$download_url = $replace -creplace "\$`(\d+)",$matches[$1]
 		# This one is not working :(
 		# I have a temporary fix that only allows ONE match. However, in most cases we only need one match.
 		$download_url = $replace -creplace "\$`(\d+)",$matches[1]
