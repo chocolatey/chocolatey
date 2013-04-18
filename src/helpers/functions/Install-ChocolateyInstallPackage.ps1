74a73242f7d98c57795c2d501f9113182b65f59c
Xin Huang <xinhuang@thoughtworks.com>
2013-04-18 21:02:13 +0800

    fix msu can not start.

--- src/helpers/functions/Install-ChocolateyInstallPackage.ps1
+++ src/helpers/functions/Install-ChocolateyInstallPackage.ps1
@@ -77,11 +77,13 @@ param(
   }
 
   if($fileType -like 'msu') {
+
     if ($overrideArguments) {
-      Start-ChocolateyProcessAsAdmin "$additionalInstallArgs" $file -validExitCodes $validExitCodes
+      $msuArgs = "$file $additionalInstallArgs"
     } else {
-      Start-ChocolateyProcessAsAdmin "$silentArgs $additionalInstallArgs" $file -validExitCodes $validExitCodes
+      $msuArgs = "$file $silentArgs $additionalInstallArgs"
     }
+    Start-ChocolateyProcessAsAdmin "$msuArgs" 'wusa.exe' -validExitCodes $validExitCodes
   }
 
   write-host "$packageName has been installed."
