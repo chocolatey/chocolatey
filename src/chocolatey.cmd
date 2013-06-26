1d1e5f7d868b7f13d47b06fb5e3ab995015afd2c
Redsandro Media <redsandro@gmail.com>
2013-06-26 02:40:15 +0200

    Added batch mode to chocolatey.cmd - cinst multiple packages

--- src/chocolatey.cmd
+++ src/chocolatey.cmd
@@ -2,15 +2,31 @@
 
 SET DIR=%~dp0%
 
+REM Handle empty parameter here because in the loop empty means 'end loop goto eof'
+if '%1'=='' GOTO usage
+
+REM Help
 if '%1'=='/?' goto usage
 if '%1'=='-?' goto usage
 if '%1'=='?' goto usage
 if '%1'=='/help' goto usage
 if '%1'=='help' goto usage
+if '%1'=='--help' goto usage
 
-@PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%DIR%chocolatey.ps1' %*"
 
+set numArgs=0
+for %%n in (%*) do Set /A numArgs+=1
+if '%numArgs%' GTR '2' echo Installing packages in batch mode.
+
+FOR %%P IN (%*) DO (
+	REM Installer
+	if not '%%P' == 'install' (
+		if '%numArgs%' GTR '2' echo Installing %%P ...
+		@PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%DIR%chocolatey.ps1' install %%P"
+	)
+)
 goto :eof
-:usage
 
+:usage
+REM Display help
 @PowerShell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%DIR%chocolatey.ps1' help"
