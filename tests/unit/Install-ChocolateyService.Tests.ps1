e6e507d4aed6df1bf98d1cf25c9d9b99487118de
030 <r.l.vanbreukelen@gmail.com>
2014-07-05 00:40:02 +0200

    Work on #489. Creating Pester test for Install-ChocolateyService

--- tests/unit/Install-ChocolateyService.Tests.ps1
+++ tests/unit/Install-ChocolateyService.Tests.ps1
@@ -8,7 +8,47 @@ Describe "Install-ChocolateyService" {
 	Context "When no packageName parameter is passed to this function" {
 		Mock Write-ChocolateyFailure
 		
-		Install-ChocolateyService
+		Install-ChocolateyService -packageName "TestTargetPath" -serviceName "TestWorkingDiectory" -createServiceCommand "TestArguments" -availablePort "1"
+		
+		It "should return an error" {
+			Assert-MockCalled Write-ChocolateyFailure -parameterFilter { $failureMessage  -eq "Missing PackageName input parameter." }
+		}
+	}
+	
+	Context "When no serviceName parameter is passed to this function" {
+		Mock Write-ChocolateyFailure
+		
+		Install-ChocolateyService -packageName "TestTargetPath" -serviceName "TestWorkingDiectory" -createServiceCommand "TestArguments" -availablePort "1"
+		
+		It "should return an error" {
+			Assert-MockCalled Write-ChocolateyFailure -parameterFilter { $failureMessage  -eq "Missing ServiceName input parameter." }
+		}
+	}
+
+	Context "When no packageName parameter is passed to this function" {
+		Mock Write-ChocolateyFailure
+		
+		Install-ChocolateyService -packageName "TestTargetPath" -serviceName "TestWorkingDiectory" -createServiceCommand "TestArguments" -availablePort "1"
+		
+		It "should return an error" {
+			Assert-MockCalled Write-ChocolateyFailure -parameterFilter { $failureMessage  -eq "Missing PackageName input parameter." }
+		}
+	}
+
+	Context "When no packageName parameter is passed to this function" {
+		Mock Write-ChocolateyFailure
+		
+		Install-ChocolateyService -packageName "TestTargetPath" -serviceName "TestWorkingDiectory" -createServiceCommand "TestArguments" -availablePort "1"
+		
+		It "should return an error" {
+			Assert-MockCalled Write-ChocolateyFailure -parameterFilter { $failureMessage  -eq "Missing PackageName input parameter." }
+		}
+	}	
+	
+	Context "When availablePort parameter is passed in String format to this function" {
+		Mock Write-ChocolateyFailure
+		
+		Install-ChocolateyService -packageName "TestTargetPath" -serviceName "TestWorkingDiectory" -createServiceCommand "TestArguments" -availablePort "1"
 		
 		It "should return an error" {
 			Assert-MockCalled Write-ChocolateyFailure -parameterFilter { $failureMessage  -eq "Missing PackageName input parameter." }
