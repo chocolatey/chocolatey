3df0334eb2ab693fb90a49dc6433ad8f7e0390fc
030 <r.l.vanbreukelen@gmail.com>
2014-07-06 02:12:01 +0200

    (GH-489) Functions inside function issue solved

--- src/helpers/functions/Install-ChocolateyService.ps1
+++ src/helpers/functions/Install-ChocolateyService.ps1
@@ -54,36 +54,24 @@ param(
     Write-ChocolateyFailure "Install-ChocolateyService" "Missing CreateServiceCommand input parameter."
     return
   }  
-  
-#  if(!$availablePort) {
-#    Write-ChocolateyFailure "Install-ChocolateyService" "Missing AvailablePort input parameter."
-#    return
-#  }    
 
   $service = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
+
+  try {
+    if ($service) {
+      Write-Host "$serviceName service already exists and will be removed"
+      stop-service $serviceName
+      $service.delete()      
+    }
   
-  function createService() {
     if (get-command $createServiceCommand -erroraction silentlycontinue) {
       Write-Host "$packageName service will be installed"
       & $createServiceCommand install $serviceName
     } else {
       Write-ChocolateyFailure 'Install-ChocolateyService' "createServiceCommand $createServiceCommand is incorrect."
       return  
-    }
-  }
+    }  
 
-  function startService() {
-#    if (serviceExist -and availablePort) {
-    if (serviceExist) {
-      Write-Host "$packageName service will be started"
-      start-service $serviceName
-    } else {
-      Write-ChocolateyFailure "Install-ChocolateyService" "service $serviceName does not exist."
-      return
-    }
-  }
-  
-  function availablePort() {
     if($availablePort) {
       $listeningStatePort = Get-NetTCPConnection -State Listen | Where-Object {$_.LocalAddress -eq "0.0.0.0" -and $_.LocalPort -eq "$availablePort"}
       if (!$listeningStatePort) {
@@ -92,88 +80,16 @@ param(
         Write-ChocolateyFailure "Install-ChocolateyService" "$availablePort is in LISTENING state and not available."
         return
       }
-	}
-  }
-
-  function serviceExist() {
-    $service
-  }
-
-  function deleteService() {
-    if (serviceExist) {
-      Write-Host "$serviceName service already exists and will be removed"
-      stop-service $serviceName
-      $service.delete()      
-	} else {
-      return $TRUE
+	}	
+	
+    if ($service) {
+      Write-Host "$packageName service will be started"
+      start-service $serviceName
+    } else {
+      Write-ChocolateyFailure "Install-ChocolateyService" "service $serviceName does not exist."
+      return
     }	
+  } catch {
+    Write-ChocolateyFailure "Install-ChocolateyService" "There were errors attempting to create the $packageName service. The error message was '$_'."
   }
-
-  deleteService
-  createService
-  startService
-  
-  
-  
-  
-  
-  
-  
-#  function installService() {
-#    if (serviceExists) {
-#      Write-Host "$packageName service will be installed"
-#	  if ( get-command helloworld -erroraction silentlycontinue ) {
-#        & $createServiceCommand install $serviceName
-#      } else {
-#        Write-ChocolateyFailure 'Install-ChocolateyService' "createServiceCommand $createServiceCommand is incorrect"
-#        return
-#      }
-#    } else {
-#      Write-ChocolateyFailure 'Install-ChocolateyService' "service $serviceName cannot be installed"
-#      return
-#    }
-#  }
-
-#  function startService() {
-#    if (portAvailable) {
-#      Write-Host "$packageName service will be started"
-#	  $service = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
-#      if ($service -match "$serviceName") {
-#        start-service $serviceName
-#      } else {
-#        Write-ChocolateyFailure "Install-ChocolateyService" "service $serviceName does not exist."
-#        return
-#      }
-#    } else {
-#      Write-ChocolateyFailure 'Install-ChocolateyService' "service $serviceName cannot be started"
-#      return
-#    }
-#  }
-
-#  function portAvailable() {
-#    Try {
-#      $connection = (New-Object Net.Sockets.TcpClient)
-#      $connection.Connect("127.0.0.1",$port)
-#      return $FALSE
-#    } Catch {
-#      return $TRUE
-#    }
-#  }
-
-#  function serviceExists() {
-#    $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
-#    if ($service.Length -gt 0) {
-#      Write-Host "$serviceName service already exists and will be removed"
-#      stop-service $serviceName
-#      $service = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
-#      $service.delete()
-#      return $TRUE
-#    } else {
-#      return $TRUE
-#    }
-#  }  
-
-#  installService
-
-#  startService
 }
